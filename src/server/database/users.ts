98fe5437441aa205cfbafc7f4a7cc346b7209fc5
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-06-21 22:09:25 +0100

    :bricks: begin to implement User database class

--- src/server/database/users.ts
+++ src/server/database/users.ts
@@ -1,4 +1,4 @@
-import { User } from "@/server/common/models";
+import { User as UserModel, UserRole } from "@/server/common/models";
 import database from "./";
 
 interface userIdentifier {
@@ -6,10 +6,41 @@ interface userIdentifier {
   full_name: string,
 }
 
-export function listUsers(): userIdentifier[] {
-  throw new Error("Not implemented.");
-}
+export default class User implements UserModel {
+  checked_in: boolean;
+  created_at: Date;
+  email: string;
+  full_name: string;
+  id: number;
+  preferred_name: string;
+  role: keyof typeof UserRole;
+  updated_at: Date;
 
-export function getUser(id: number): User {
-  throw new Error("Not implemented.");
+  constructor() {
+  }
+  static async listUsers(): Promise<userIdentifier[]> {
+    throw new Error("Not implemented.");
+  }
+
+  static async getUser(id: number): Promise<User> {
+    throw new Error("Not implemented.");
+  }
+
+  static async findUserByEmail(email: string): Promise<User> {
+    throw new Error("Not implemented.");
+  }
+
+  static async createNewUser(): Promise<void> {
+    throw new Error("Not implemented.");
+  }
+
+  static async updateUserDetails(): Promise<void> {
+    throw new Error("Not implemented.");
+  }
+
+  static async updateUserPassword(): Promise<void> {
+    throw new Error("Not implemented.");
+  }
+
+
 }
