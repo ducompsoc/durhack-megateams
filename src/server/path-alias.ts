e64a48a57331925c1272058eb521738db07c2292
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-06-22 01:46:00 +0100

    :heavy_plus_sign: :wrench: install tsc-alias and configure to allow paths after building

---
+++ src/server/path-alias.ts
@@ -0,0 +1,13 @@
+import { replaceTscAliasPaths } from "tsc-alias";
+
+function registerModuleAliases() {
+  // check if running in ts-node; do nothing if so
+  // @ts-ignore
+  if (process[Symbol.for("ts-node.register.instance")]) return;
+
+  void replaceTscAliasPaths({
+    configFile: "tsconfig.server.json",
+  });
+}
+
+registerModuleAliases();
