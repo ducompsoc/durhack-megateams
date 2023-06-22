261bc7de985d246641eafde7b22397e8c8127e57
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-06-22 02:07:01 +0100

    :label: fix type errors

--- src/server/common/errors.ts
+++ src/server/common/errors.ts
@@ -1,12 +1,20 @@
-export class NullError extends Error {
-  constructor(message) {
+class CustomError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = "CustomError";
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+export class NullError extends CustomError {
+  constructor(message?: string) {
     super(message);
     this.name = "NullError";
   }
 }
 
-export class ValueError extends Error {
-  constructor(message) {
+export class ValueError extends CustomError {
+  constructor(message?: string) {
     super(message);
     this.name = "ValueError";
   }
