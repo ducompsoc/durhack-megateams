0ad5ae0f84818f47bf009258d7f0bf7d499e3c67
Jacob Wrenn <jacob@wrenn.me>
2023-06-24 21:09:43 +0100

    finish basic register form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -87,15 +87,15 @@ export default function Register() {
 
           <div>
             <label
-              htmlFor="full-name"
+              htmlFor="preferred-name"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Full Name
+              Preferred Name
             </label>
             <div className="mt-2">
               <input
-                id="full-name"
-                name="full-name"
+                id="preferred-name"
+                name="preferred-name"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
@@ -105,15 +105,51 @@ export default function Register() {
 
           <div>
             <label
-              htmlFor="full-name"
+              htmlFor="age"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Full Name
+              Age
             </label>
             <div className="mt-2">
               <input
-                id="full-name"
-                name="full-name"
+                id="age"
+                name="age"
+                type="number"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="phone-number"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Phone Number
+            </label>
+            <div className="mt-2">
+              <input
+                id="phone-number"
+                name="phone-number"
+                type="tel"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="discord-id"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Discord ID
+            </label>
+            <div className="mt-2">
+              <input
+                id="discord-id"
+                name="discord-id"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
@@ -123,15 +159,15 @@ export default function Register() {
 
           <div>
             <label
-              htmlFor="full-name"
+              htmlFor="discord-name"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Full Name
+              Discord Name
             </label>
             <div className="mt-2">
               <input
-                id="full-name"
-                name="full-name"
+                id="discord-name"
+                name="discord-name"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
@@ -141,15 +177,15 @@ export default function Register() {
 
           <div>
             <label
-              htmlFor="full-name"
+              htmlFor="university"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Full Name
+              University
             </label>
             <div className="mt-2">
               <input
-                id="full-name"
-                name="full-name"
+                id="university"
+                name="university"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
@@ -159,15 +195,38 @@ export default function Register() {
 
           <div>
             <label
-              htmlFor="full-name"
+              htmlFor="graduation-year"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Full Name
+              Graduation Year
+            </label>
+            <div className="mt-2">
+              <select
+                id="graduation-year"
+                name="graduation-year"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
+              >
+                <option value={2024}>2024</option>
+                <option value={2025}>2025</option>
+                <option value={2026}>2026</option>
+                <option value={2027}>2027</option>
+                <option value={2028}>2028</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="ethnicity"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Ethnicity
             </label>
             <div className="mt-2">
               <input
-                id="full-name"
-                name="full-name"
+                id="ethnicity"
+                name="ethnicity"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
@@ -177,15 +236,15 @@ export default function Register() {
 
           <div>
             <label
-              htmlFor="full-name"
+              htmlFor="gender"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Full Name
+              Gender
             </label>
             <div className="mt-2">
               <input
-                id="full-name"
-                name="full-name"
+                id="gender"
+                name="gender"
                 type="text"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
@@ -194,93 +253,41 @@ export default function Register() {
           </div>
 
           <div>
-            <label
-              htmlFor="full-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Full Name
-            </label>
-            <div className="mt-2">
+            <div className="mt-2 flex items-center">
               <input
-                id="full-name"
-                name="full-name"
-                type="text"
+                id="hackathons"
+                name="hackathons"
+                type="checkbox"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
+                className="text-accent bg-gray-100 border-gray-300 rounded focus:ring-accent focus:ring-2"
               />
+            <label
+              htmlFor="hackathons"
+              className="block text-sm font-medium leading-6 text-gray-900 ml-4"
+            >
+              Hackathons UK Consent
+            </label>
             </div>
+            <p className="text-sm mt-2">Allow DurHack to share anonymised details with Hackathons UK.</p>
           </div>
 
           <div>
-            <label
-              htmlFor="full-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Full Name
-            </label>
-            <div className="mt-2">
+            <div className="mt-2 flex items-center">
               <input
-                id="full-name"
-                name="full-name"
-                type="text"
+                id="hackathons-marketing"
+                name="hackathons-marketing"
+                type="checkbox"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
+                className="text-accent bg-gray-100 border-gray-300 rounded focus:ring-accent focus:ring-2"
               />
-            </div>
-          </div>
-
-          <div>
             <label
-              htmlFor="full-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
+              htmlFor="hackathons-marketing"
+              className="block text-sm font-medium leading-6 text-gray-900 ml-4"
             >
-              Full Name
+              Hackathons UK Marketing Consent
             </label>
-            <div className="mt-2">
-              <input
-                id="full-name"
-                name="full-name"
-                type="text"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
-              />
             </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="full-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Full Name
-            </label>
-            <div className="mt-2">
-              <input
-                id="full-name"
-                name="full-name"
-                type="text"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="full-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Full Name
-            </label>
-            <div className="mt-2">
-              <input
-                id="full-name"
-                name="full-name"
-                type="text"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
-              />
-            </div>
+            <p className="text-sm mt-2">Allow Hackathons UK to contact you for marketing purposes.</p>
           </div>
 
           <div>
