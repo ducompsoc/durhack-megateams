832ab1c7668107fbca9a8f4e0dd62960b03eeb26
Skye <skyecarroll42@gmail.com>
2023-06-26 15:58:21 +0100

    ⏪ enable port option for connecting to database

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -4,7 +4,7 @@ import mysql from "mysql2";
 const pool = mysql.createPool({
   connectionLimit: 100,
   host     : process.env.DATABASE_HOST,
-  // port     : Number(process.env.DATABASE_PORT),
+  port     : Number(process.env.DATABASE_PORT),
   user     : process.env.DATABASE_USER,
   password : process.env.DATABASE_PASSWORD,
   database : process.env.DATABASE_NAME
