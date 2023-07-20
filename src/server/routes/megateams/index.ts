be0cd139ee63a0dae5d44678479f351e1b107672
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 19:49:10 +0100

    wireframe megateams router

--- src/server/routes/megateams/index.ts
+++ src/server/routes/megateams/index.ts
@@ -1,5 +1,20 @@
 import { Router as ExpressRouter } from "express";
 
+import * as handlers from "./megateam_handlers";
+import { HandleMethodNotAllowed } from "@/server/common/middleware";
+
+
 const megateams_router = ExpressRouter();
+
+megateams_router.route("/")
+  .get(handlers.getMegateamsList)
+  .post(handlers.createMegateam)
+  .all(HandleMethodNotAllowed);
+
+megateams_router.route("/:megateam_id")
+  .get(handlers.getMegateamDetails)
+  .patch(handlers.patchMegateamDetails)
+  .delete(handlers.deleteMegateam)
+  .all(HandleMethodNotAllowed);
 
 export default megateams_router;
