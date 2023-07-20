f74cac33ea2a6e4a32f6aef086e9da92ef835a61
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 19:37:16 +0100

    wireframe qrcodes router

--- src/server/routes/qr_codes/index.ts
+++ src/server/routes/qr_codes/index.ts
@@ -1,5 +1,20 @@
 import { Router as ExpressRouter } from "express";
 
+import * as handlers from "./qr_code_handlers";
+import { HandleMethodNotAllowed } from "@/server/common/middleware";
+
+
 const qr_codes_router = ExpressRouter();
+
+qr_codes_router.route("/")
+  .get(handlers.getQrCodeList)
+  .post(handlers.createQrCode)
+  .all(HandleMethodNotAllowed);
+
+qr_codes_router.route("/:qr_code_id")
+  .get(handlers.getQrCodeDetails)
+  .patch(handlers.patchQrCodeDetails)
+  .delete(handlers.deleteQrCode)
+  .all(HandleMethodNotAllowed);
 
 export default qr_codes_router;
