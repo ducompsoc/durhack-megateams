8a03cc9a0b56138755d83b283199c85330c8aeff
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 18:05:01 +0100

    rename next.ts -> nextApp.ts

--- src/server/common/next.ts
+++
@@ -1,6 +0,0 @@
-import next from "next";
-
-const next_app = next({ dev: process.env.NODE_ENV !== "production" });
-
-export default next_app;
-export const handle_next_app_request = next_app.getRequestHandler();
