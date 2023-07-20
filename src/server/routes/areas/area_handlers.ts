802e155c0a336e7478e9bf71e73800ebc9a1ab2a
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 19:50:52 +0100

    wireframe areas router

---
+++ src/server/routes/areas/area_handlers.ts
@@ -0,0 +1,26 @@
+import createHttpError from "http-errors";
+import { Request, Response } from "express";
+
+import { NullError } from "@/server/common/errors";
+import Area from "@/server/database/area";
+
+
+export async function getAreasList(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function createArea(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function getAreaDetails(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function patchAreaDetails(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function deleteArea(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
--- src/server/routes/areas/index.ts
+++ src/server/routes/areas/index.ts
@@ -1,5 +1,20 @@
 import { Router as ExpressRouter } from "express";
 
+import * as handlers from "./area_handlers";
+import { HandleMethodNotAllowed } from "@/server/common/middleware";
+
+
 const areas_router = ExpressRouter();
+
+areas_router.route("/")
+  .get(handlers.getAreasList)
+  .post(handlers.createArea)
+  .all(HandleMethodNotAllowed);
+
+areas_router.route("/:area_id")
+  .get(handlers.getAreaDetails)
+  .patch(handlers.patchAreaDetails)
+  .delete(handlers.deleteArea)
+  .all(HandleMethodNotAllowed);
 
 export default areas_router;
