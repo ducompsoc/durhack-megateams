dae43665af180db1bf3d80df8f11128f64663a7a
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 19:34:01 +0100

    wireframe teams router

--- src/server/routes/teams/index.ts
+++ src/server/routes/teams/index.ts
@@ -1,5 +1,20 @@
 import { Router as ExpressRouter } from "express";
 
+import * as handlers from "./team_handlers";
+import { HandleMethodNotAllowed } from "@/server/common/middleware";
+
+
 const teams_router = ExpressRouter();
+
+teams_router.route("/")
+  .get(handlers.getTeamsList)
+  .post(handlers.createTeam)
+  .all(HandleMethodNotAllowed);
+
+teams_router.route("/:team_id")
+  .get(handlers.getTeamDetails)
+  .patch(handlers.patchTeamDetails)
+  .delete(handlers.deleteTeam)
+  .all(HandleMethodNotAllowed);
 
 export default teams_router;
