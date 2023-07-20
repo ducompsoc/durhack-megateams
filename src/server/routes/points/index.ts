e7d16b808381bf587f5406431685c84126605cf5
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 19:44:46 +0100

    wireframe points router

--- src/server/routes/points/index.ts
+++ src/server/routes/points/index.ts
@@ -1,5 +1,20 @@
 import { Router as ExpressRouter } from "express";
 
+import * as handlers from "./point_handlers";
+import { HandleMethodNotAllowed } from "@/server/common/middleware";
+
+
 const points_router = ExpressRouter();
+
+points_router.route("/")
+  .get(handlers.getPointsList)
+  .post(handlers.createPoint)
+  .all(HandleMethodNotAllowed);
+
+points_router.route("/:point_id")
+  .get(handlers.getPointDetails)
+  .patch(handlers.patchPointDetails)
+  .delete(handlers.deletePoint)
+  .all(HandleMethodNotAllowed);
 
 export default points_router;
---
+++ src/server/routes/points/point_handlers.ts
@@ -0,0 +1,26 @@
+import createHttpError from "http-errors";
+import { Request, Response } from "express";
+
+import { NullError } from "@/server/common/errors";
+import Point from "@/server/database/point";
+
+
+export async function getPointsList(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function createPoint(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function getPointDetails(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function patchPointDetails(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
+
+export async function deletePoint(request: Request, response: Response): Promise<void> {
+  throw new createHttpError.NotImplemented();
+}
