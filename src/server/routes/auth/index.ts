2297fb31e55c2960750771eeb2ef900ab7dd3c08
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-20 20:45:28 +0100

    :bug: User cannot extend itself; import the database user as an alias

--- src/server/routes/auth/index.ts
+++ src/server/routes/auth/index.ts
@@ -2,7 +2,7 @@ import { Router as ExpressRouter } from "express";
 import passport from "passport";
 import Local from "passport-local";
 
-import User from "@server/database/user";
+import SequelizeUser from "@server/database/user";
 import { NullError } from "@server/common/errors";
 import { HandleMethodNotAllowed } from "@server/common/middleware";
 
@@ -11,7 +11,7 @@ import { localVerifyFunction } from "./auth_util";
 
 declare global {
   namespace Express {
-    interface User extends User {}
+    interface User extends SequelizeUser {}
   }
 }
 
@@ -32,7 +32,7 @@ passport.deserializeUser<SerializedUser>(async function(identifier, callback) {
     return callback(null, null);
   }
   try {
-    return callback(null, await User.findByPk(identifier.id, { rejectOnEmpty: new NullError() }));
+    return callback(null, await SequelizeUser.findByPk(identifier.id, { rejectOnEmpty: new NullError() }));
   } catch (error) {
     if (error instanceof NullError) return callback(null, null);
     return callback(error);
