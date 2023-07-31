5c667bacae0025cb83fe91e6ca8a4f7cc97a6730
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-31 18:40:42 +0100

    fix webpack build error

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,7 +1,9 @@
+const isClient = typeof window !== "undefined";
+
 // should be replaced with an (environment?) variable if we separate the API and frontend processes
-export const megateams_api_base_url = window.location.origin;
+export const megateams_api_base_url = isClient? window.location.origin : "";
 
 export function makeMegateamsApiRequest(endpoint: string, options?: RequestInit): Request {
-    const api_endpoint_url = new URL(endpoint, megateams_api_base_url)
+    const api_endpoint_url = new URL("/api" + endpoint, megateams_api_base_url)
     return new Request(api_endpoint_url, options);
 }
