45a6cccb7c07873dd50f86e0dec2f893d31b01ef
tameTNT <30503695+tameTNT@users.noreply.github.com>
2023-08-02 13:24:01 +0100

    🚨 eslint fix semis and indents

--- src/app/csrf-test/page.tsx
+++ src/app/csrf-test/page.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
 import { attachCsrfTokenToRequest } from "@/app/lib/csrf";
 import { makeMegateamsApiRequest } from "@/app/lib/api";
 
 async function send_login_request() {
-    const request = makeMegateamsApiRequest("/auth/login", { method: "POST" })
-    await attachCsrfTokenToRequest(request);
-    await fetch(request);
+  const request = makeMegateamsApiRequest("/auth/login", { method: "POST" });
+  await attachCsrfTokenToRequest(request);
+  await fetch(request);
 }
 
 async function send_bad_login_request() {
-    const request = makeMegateamsApiRequest("/auth/login", { method: "POST" })
-    await fetch(request);
+  const request = makeMegateamsApiRequest("/auth/login", { method: "POST" });
+  await fetch(request);
 }
 
 export default function CsrfTestPage() {
-    return <main>
-        <button onClick={send_login_request}>Send Login Request</button>
-        <button onClick={send_bad_login_request}>Send Forbidden Login Request</button>
-    </main>
+  return <main>
+    <button onClick={send_login_request}>Send Login Request</button>
+    <button onClick={send_bad_login_request}>Send Forbidden Login Request</button>
+  </main>;
 }
