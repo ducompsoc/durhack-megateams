8432b45bc4bd1b9e067fcd9f1c98e435cf45e389
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-11 04:36:31 +0100

    add common middleware for getting IDs from routes

--- src/server/common/middleware.ts
+++ src/server/common/middleware.ts
@@ -7,3 +7,59 @@ export function handleMethodNotAllowed() {
 export function handleNotImplemented() {
   throw new createHttpError.NotImplemented();
 }
+
+export function handleFailedAuthentication(request: Request) {
+  if (request.user) {
+    throw new createHttpError.Forbidden();
+  }
+  throw new createHttpError.Unauthorized();
+}
+
+export function mutateRequestValue<T>(getter: (request: Request) => unknown, mutator: (value: unknown) => T, setter: (response: Response, value: T) => void) {
+  return function (request: Request, response: Response, next: NextFunction) {
+    const valueToMutate = getter(request);
+    const mutatedValue = mutator(valueToMutate);
+    setter(response, mutatedValue);
+    next();
+  };
+}
+
+function getRouteParameter(key: string) {
+  return function (request: Request): unknown {
+    return request.params[key];
+  };
+}
+
+function getQueryParameter(key: string) {
+  return function (request: Request): unknown {
+    return request.query[key];
+  };
+}
+
+function setLocalValue<T>(key: string) {
+  return function (response: Response, value: T): void {
+    response.locals[key] = value;
+  };
+}
+
+function validateID(value: unknown): number {
+  if (typeof value !== "string") {
+    throw new TypeError(`'${value}' should be a single string value.`);
+  }
+
+  const num = Number(value);
+
+  if (!(Number.isInteger(num) && num >= 0)) {
+    throw new ValueError(`'${value}' is not a valid ID.`);
+  }
+
+  return num;
+}
+
+export function parseRouteId(key: string) {
+  return mutateRequestValue(
+    getRouteParameter(key),
+    validateID,
+    setLocalValue(key)
+  );
+}
