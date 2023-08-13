2d88c86e7a3c62689c7d0d02a327d1fe1dbd170c
tameTNT <30503695+tameTNT@users.noreply.github.com>
2023-08-13 23:36:57 +0100

    ✨ Implement createPoint
    Allows admin users to manually add points.
    Prevents an origin_qrcode_id attribute from being added to ensure
    it is clear when points have been added manually by admins.

--- src/server/routes/points/point_handlers.ts
+++ src/server/routes/points/point_handlers.ts
@@ -1,9 +1,19 @@
 import createHttpError from "http-errors";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import {
+  ForeignKeyConstraintError as SequelizeForeignKeyConstraintError,
+  ValidationError as SequelizeValidationError
+} from "sequelize";
 
-import { NullError } from "@server/common/errors";
+import { NullError, ValueError } from "@server/common/errors";
 import Point from "@server/database/point";
 import User from "@server/database/user";
+
+const user_attributes = Point.getAttributes();
+const allowed_create_fields = new Set(Object.keys(user_attributes));
+
+allowed_create_fields.delete("createdAt");
+allowed_create_fields.delete("updatedAt");
 
 
 /**
@@ -28,8 +38,46 @@ export async function getPointsList(request: Request, response: Response): Promi
   });
 }
 
-export async function createPoint(request: Request, response: Response): Promise<void> {
-  throw new createHttpError.NotImplemented();
+/**
+ * Handles an authenticated admin POST request to /points to manually add points to the database.
+ *
+ * @param request
+ * @param response
+ * @param next
+ */
+export async function createPoint(request: Request, response: Response, next: NextFunction): Promise<void> {
+  // Point creation via QR code (for non-admins) is handled by a separate endpoint
+  if (!response.locals.isAdminRequest) {
+    return next();
+  }
+
+  const invalid_fields = Object.keys(request.body).filter((key) => !allowed_create_fields.has(key));
+  if (invalid_fields.length > 0) {
+    throw new ValueError(`Invalid field name(s) provided: ${invalid_fields}`);
+  }
+
+  if ("origin_qrcode_id" in request.body) {
+    throw new createHttpError.UnprocessableEntity("You should not specify an origin QR (`origin_qrcode_id`) when manually adding points.");
+  }
+
+  let new_instance;
+  try {
+    new_instance = await Point.create(request.body);
+  } catch (error) {
+    if (error instanceof SequelizeValidationError) {
+      throw new createHttpError.BadRequest(error.message);
+    } else if (error instanceof SequelizeForeignKeyConstraintError) {
+      if (error.fields instanceof Array) {
+        throw new createHttpError.BadRequest(`Invalid foreign key(s) provided for field(s): ${error.fields.join(", ")}`);
+      } else {
+        throw new createHttpError.BadRequest(error.message);
+      }
+    }
+    throw error;
+  }
+
+  response.status(200);
+  response.json({ status: response.statusCode, message: "OK", data: new_instance });
 }
 
 export async function getPointDetails(request: Request, response: Response): Promise<void> {
