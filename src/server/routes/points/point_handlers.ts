96460f767167a76c97a37e3c565f03ab1ebaf629
tameTNT <30503695+tameTNT@users.noreply.github.com>
2023-08-13 22:15:10 +0100

    ✨ Implement getPointsList
    Returns a basic list of all points in the database and their redeemer ID

--- src/server/routes/points/point_handlers.ts
+++ src/server/routes/points/point_handlers.ts
@@ -3,10 +3,29 @@ import { Request, Response } from "express";
 
 import { NullError } from "@server/common/errors";
 import Point from "@server/database/point";
+import User from "@server/database/user";
 
 
+/**
+ * Handles a GET request to /points.
+ * For transparency, returns a list of points in the database with their values and redeemer's ID.
+ *
+ * @param request
+ * @param response
+ */
 export async function getPointsList(request: Request, response: Response): Promise<void> {
-  throw new createHttpError.NotImplemented();
+  const result = await Point.findAll({
+    attributes: [["point_id", "id"], "value"],
+    include: [
+      { model: User, attributes: [["user_id", "id"]] },
+    ]
+  });
+  response.status(200);
+  response.json({
+    status: 200,
+    message: "OK",
+    points: result,
+  });
 }
 
 export async function createPoint(request: Request, response: Response): Promise<void> {
