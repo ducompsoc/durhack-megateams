825bb93bc491ef4f983ee438db7e5e424ce43bba
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-13 16:37:56 +0100

    tidy imports and import database before api_router (resolves ModelNotInitializedError)

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,13 @@
-import "./common/config";
-import express from "express";
 import "./path-alias";
+import "./common/config";
+
+import express from "express";
 import passport from "passport";
+
 import session from "./common/session";
+import sequelize, { ensureDatabaseExists } from "./database";
 import next_app, { handle_next_app_request } from "./common/nextApp";
 import api_router from "./routes";
-import sequelize, { ensureDatabaseExists } from "./database";
 
 const dev = process.env.NODE_ENV !== "production";
 process.env.TZ = "Europe/London";
