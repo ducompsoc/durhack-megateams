472ef739a817fd52cc94915eda6ccf9d150037e0
tameTNT <30503695+tameTNT@users.noreply.github.com>
2023-08-17 22:12:56 +0100

    🐛 Fix lack of next function mentioned in previous commit
    Endpoints which require admin authentication should include a next
    function (handleFailedAuthentication) in this case handling user error.

--- src/server/routes/points/index.ts
+++ src/server/routes/points/index.ts
@@ -1,6 +1,10 @@
 import { Router as ExpressRouter } from "express";
 
-import { handleMethodNotAllowed, parseRouteId } from "@server/common/middleware";
+import {
+  handleFailedAuthentication,
+  handleMethodNotAllowed,
+  parseRouteId
+} from "@server/common/middleware";
 
 import * as handlers from "./point_handlers";
 
@@ -10,14 +14,14 @@ const points_router = ExpressRouter();
 
 points_router.route("/")
   .get(handlers.getPointsList)
-  .post(handlers.createPoint)
+  .post(handlers.createPoint, handleFailedAuthentication)
   .all(handleMethodNotAllowed);
 
 points_router.route("/:point_id")
   .all(parseRouteId("point_id"))
   .get(handlers.getPointDetails)
-  .patch(handlers.patchPointDetails)
-  .delete(handlers.deletePoint)
+  .patch(handlers.patchPointDetails, handleFailedAuthentication)
+  .delete(handlers.deletePoint, handleFailedAuthentication)
   .all(handleMethodNotAllowed);
 
 export default points_router;
