7d5c3f2ed0c47231510212b957542e7800c5c328
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-17 21:15:13 +0100

    fix handler imports and import from user util

--- src/server/routes/users/index.ts
+++ src/server/routes/users/index.ts
@@ -1,19 +1,10 @@
-import { NextFunction, Request, Response, Router as ExpressRouter } from "express";
+import { Router as ExpressRouter } from "express";
 
-import * as handlers from "./user_handlers";
 import { handleFailedAuthentication, handleMethodNotAllowed, parseRouteId } from "@server/common/middleware";
-import createHttpError from "http-errors";
 
+import handlers from "./user_handlers";
+import { useSelfId } from "./user_util";
 
-function useSelfId(request: Request, response: Response, next: NextFunction) {
-  if (!request.user) {
-    throw new createHttpError.Unauthorized();
-  }
-
-  response.locals.user_id = request.user.id;
-  response.locals.isSelfRequest = true;
-  next();
-}
 
 const users_router = ExpressRouter();
 
@@ -30,7 +21,7 @@ users_router.route("/me")
   .all(handleMethodNotAllowed);
 
 users_router.route("/:user_id")
-  .all(parseRouteId("user_id"), handlers.setSelfRequestFlag)
+  .all(parseRouteId("user_id"))
   .get(handlers.getUserDetailsAsAdmin, handlers.getUserDetailsDefault)
   .patch(handlers.patchUserDetailsAsAdmin, handlers.patchMyUserDetails, handleFailedAuthentication)
   .delete(handlers.deleteUserAsAdmin, handleFailedAuthentication)
