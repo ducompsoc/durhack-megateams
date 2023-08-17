d414ef0a67982680de6bc572b11319a23c57b829
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-17 20:49:32 +0100

    add doc comments

--- src/server/common/decorators.ts
+++ src/server/common/decorators.ts
@@ -3,6 +3,15 @@ import {UserRole} from "@server/common/model_enums";
 
 type ICondition = (request: Request, response: Response) => boolean;
 
+/**
+ * Returns a decorator that can be applied to an Express request handler class method.
+ * The decorated method will call the provided condition function with Express request
+ * and response objects.
+ * If the condition returns `false`, then the handler will be skipped.
+ * If the condition returns `true`, the handler will consider the request as usual.
+ *
+ * @param condition
+ */
 export function requireCondition(condition: ICondition) {
   return function (target: any, property_key: string, descriptor: PropertyDescriptor) {
     const old_function = descriptor.value;
@@ -28,7 +37,22 @@ export function userIsLoggedIn(request: Request) {
   return !!request.user;
 }
 
+/**
+ * Decorator that ensures `request.user.role` is `UserRole.admin`.
+ */
 export const requireUserIsAdmin = requireCondition(userIsRole(UserRole.admin));
+
+/**
+ * Decorator that ensures `request.user.role` is `UserRole.volunteer`.
+ */
 export const requireUserIsVolunteer = requireCondition(userIsRole(UserRole.volunteer));
+
+/**
+ * Decorator that ensures `request.user.role` is `UserRole.sponsor`.
+ */
 export const requireUserIsSponsor = requireCondition(userIsRole(UserRole.sponsor));
+
+/**
+ * Decorator that ensures `request.user` is not null/undefined.
+ */
 export const requireLoggedIn = requireCondition(userIsLoggedIn);
