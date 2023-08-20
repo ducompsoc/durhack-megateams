294e1c337e66bf694e435f517ddac035eb5866f1
Jacob Wrenn <jacob@wrenn.me>
2023-08-20 20:02:47 +0100

    Custom name/description needed for presets

--- src/app/volunteer/volunteer.tsx
+++ src/app/volunteer/volunteer.tsx
@@ -156,6 +156,11 @@ export default function Volunteer() {
       name: "Preset",
       content: (
         <div className="dh-box p-4">
+          <input
+            type="text"
+            className="mb-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent sm:text-sm sm:leading-6"
+            placeholder="Name/Description"
+          />
           <select
             className="dh-input w-full"
             onChange={(e) =>
