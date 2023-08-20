2c3de0e4f3d4d8d92fbcc38c99b47145cc2f5da7
Jacob Wrenn <jacob@wrenn.me>
2023-08-20 11:25:31 +0100

    Complete intial QR routes

--- src/server/routes/qr_codes/qr_code_handlers.ts
+++ src/server/routes/qr_codes/qr_code_handlers.ts
@@ -12,6 +12,9 @@ import {
   requireUserIsVolunteer,
   requireUserIsSponsor,
 } from "@server/common/decorators";
+import { Op } from "sequelize";
+import sequelize from "@server/database";
+import { z } from "zod";
 
 const presets = JSON.parse(
   readFileSync(path.join(__dirname, "./QR_presets.json")).toString()
@@ -199,6 +202,118 @@ class QRHandlers {
   @requireUserIsSponsor
   async getPresetsSponsor(request: Request, response: Response) {
     this.getPresets(request, response);
+  }
+
+  async redeemQR(request: Request, response: Response) {
+    if (!request.body.hasOwnProperty("uuid")) {
+      throw new createHttpError.BadRequest();
+    }
+
+    let qr = await QRCode.findOne({
+      where: { payload: request.body.uuid },
+      include: [Point],
+      rejectOnEmpty: new createHttpError.BadRequest(),
+    });
+
+    let now = new Date();
+    if (
+      now < qr.start_time ||
+      now > qr.expiry_time ||
+      !qr.state ||
+      (qr.uses?.length || 0) >= qr.max_uses
+    ) {
+      throw new createHttpError.BadRequest();
+    }
+
+    await Point.create({
+      value: qr.points_value,
+      origin_qrcode_id: qr.id,
+      redeemer_id: request.user?.id,
+    });
+
+    response.json({
+      status: 200,
+      message: "OK",
+      points: qr.points_value,
+    });
+  }
+
+  private async getChallengeList(
+    _request: Request,
+    response: Response,
+    includeId: boolean
+  ) {
+    let challenges = await QRCode.findAll({
+      where: { challenge_rank: { [Op.ne]: null } },
+    });
+
+    challenges.sort((a, b) => a.challenge_rank! - b.challenge_rank!);
+
+    let data = challenges.map((challenge, i) => ({
+      title: challenge.name,
+      points: challenge.points_value,
+      rank: i,
+      ...(includeId ? { id: challenge.id } : {}),
+    }));
+
+    response.json({
+      status: 200,
+      message: "OK",
+      challenges: data,
+    });
+  }
+
+  @requireUserIsAdmin
+  async getChallengeListAdmin(request: Request, response: Response) {
+    await this.getChallengeList(request, response, true);
+  }
+
+  async getChallengeListUser(request: Request, response: Response) {
+    await this.getChallengeList(request, response, false);
+  }
+
+  private challengeList = z.array(
+    z.object({
+      id: z.number(),
+      rank: z.number(),
+    })
+  );
+
+  @requireUserIsAdmin
+  async reorderChallengeList(request: Request, response: Response) {
+    if (
+      !request.body.hasOwnProperty("challenges") ||
+      !this.challengeList.safeParse(request.body.challenges).success
+    ) {
+      throw new createHttpError.BadRequest();
+    }
+
+    const t = await sequelize.transaction();
+
+    try {
+      await QRCode.update(
+        { challenge_rank: null },
+        { where: { challenge_rank: { [Op.ne]: null } } }
+      );
+
+      for (let challenge of request.body.challenges) {
+        let found_challenge = await QRCode.findByPk(challenge.id, {
+          rejectOnEmpty: new NullError(),
+        });
+        found_challenge.challenge_rank = challenge.rank;
+        await found_challenge.save();
+      }
+
+      await t.commit();
+
+      response.json({
+        status: 200,
+        message: "OK",
+      });
+    } catch (error) {
+      await t.rollback();
+      throw error;
+    }
   }
 }
 
