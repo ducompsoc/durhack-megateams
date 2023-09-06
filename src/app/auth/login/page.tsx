57c8c88596fcddaf9087de2e4183e71858fc59dc
Jacob Wrenn <jacob@wrenn.me>
2023-09-06 21:22:43 +0100

    Missing text color change

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -8,7 +8,7 @@ export default function Login() {
           <Image src="/logo.png" alt="DurHack Logo" width={64} height={64} />
           <h1 className="text-4xl font-bold ml-4 font-heading">DURHACK</h1>
         </div>
-        <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-neutral-200">
           Sign in to your account
         </h2>
       </div>
