86e6f53080b6bbbb1a3e8f330a6cefe9016b1a1c
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-08 01:15:41 +0100

    bring back area routes wireframe

--- src/server/routes/areas/area_handlers.ts
+++ src/server/routes/areas/area_handlers.ts
@@ -3,10 +3,11 @@ import { Request, Response } from "express";
 import { requireUserIsAdmin } from "@server/common/decorators";
 import Area from "@server/database/area";
 import Megateam from "@server/database/megateam";
+import createHttpError from "http-errors";
 
 class AreaHandlers {
   @requireUserIsAdmin
-  async getAreas(req: Request, res: Response) {
+  async getAreasList(req: Request, res: Response): Promise<void> {
     const result = await Megateam.findAll({
       include: [Area],
     });
@@ -16,6 +17,26 @@ class AreaHandlers {
       message: "OK",
       megateams: result,
     });
+  }
+
+  @requireUserIsAdmin
+  async createArea(request: Request, response: Response): Promise<void> {
+    throw new createHttpError.NotImplemented();
+  }
+
+  @requireUserIsAdmin
+  async getAreaDetails(request: Request, response: Response): Promise<void> {
+    throw new createHttpError.NotImplemented();
+  }
+
+  @requireUserIsAdmin
+  async patchAreaDetails(request: Request, response: Response): Promise<void> {
+    throw new createHttpError.NotImplemented();
+  }
+
+  @requireUserIsAdmin
+  async deleteArea(request: Request, response: Response): Promise<void> {
+    throw new createHttpError.NotImplemented();
   }
 }
 
--- src/server/routes/areas/index.ts
+++ src/server/routes/areas/index.ts
@@ -1,12 +1,19 @@
 import { Router as ExpressRouter } from "express";
 
 import handlers from "./area_handlers";
-import { handleMethodNotAllowed } from "@server/common/middleware";
+import {handleMethodNotAllowed, parseRouteId} from "@server/common/middleware";
 
 const areas_router = ExpressRouter();
 
 areas_router.route("/")
-  .get(handlers.getAreas)
+  .get(handlers.getAreasList)
+  .post(handlers.createArea)
   .all(handleMethodNotAllowed);
+
+areas_router.route("/:area_id")
+  .all(parseRouteId("area_id"))
+  .get(handlers.getAreaDetails)
+  .patch(handlers.patchAreaDetails)
+  .delete(handlers.deleteArea);
 
 export default areas_router;
