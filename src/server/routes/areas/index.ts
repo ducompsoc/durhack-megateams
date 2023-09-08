23b332042e9e7d7123c50f78d838e4aae8eb7e28
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-08 01:16:28 +0100

    tidy imports; handle failed auth

--- src/server/routes/areas/index.ts
+++ src/server/routes/areas/index.ts
@@ -1,19 +1,26 @@
 import { Router as ExpressRouter } from "express";
 
+import {
+  handleFailedAuthentication,
+  handleMethodNotAllowed,
+  parseRouteId
+} from "@server/common/middleware";
+
 import handlers from "./area_handlers";
-import {handleMethodNotAllowed, parseRouteId} from "@server/common/middleware";
+
 
 const areas_router = ExpressRouter();
 
 areas_router.route("/")
-  .get(handlers.getAreasList)
-  .post(handlers.createArea)
+  .get(handlers.getAreasList, handleFailedAuthentication)
+  .post(handlers.createArea, handleFailedAuthentication)
   .all(handleMethodNotAllowed);
 
 areas_router.route("/:area_id")
   .all(parseRouteId("area_id"))
-  .get(handlers.getAreaDetails)
-  .patch(handlers.patchAreaDetails)
-  .delete(handlers.deleteArea);
+  .get(handlers.getAreaDetails, handleFailedAuthentication)
+  .patch(handlers.patchAreaDetails, handleFailedAuthentication)
+  .delete(handlers.deleteArea, handleFailedAuthentication)
+  .all(handleMethodNotAllowed);
 
 export default areas_router;
