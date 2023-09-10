d36628ca888984608303bfe6a32789dcc3eb52a7
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-10 21:51:51 +0100

    add - to tsdoc params

--- server/src/routes/users/user_util.ts
+++ server/src/routes/users/user_util.ts
@@ -5,9 +5,8 @@ import { requireCondition } from "@server/common/decorators";
  * Condition function that determines if the resource a user is trying to access/modify
  * is themselves.
  *
- * @param request
- * @param response
- * @private
+ * @param request -
+ * @param response -
  */
 export function userIsSelf(request: Request, response: Response): boolean {
   return (!!request.user && request.user.id === response.locals.user_id);
