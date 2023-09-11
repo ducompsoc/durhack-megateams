3d86977151607f88411ad5b3f1e3cb1d59bf0bf4
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-11 23:18:38 +0100

    add oauth routes, remove /auth/user route

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,15 +1,15 @@
 import { Router as ExpressRouter } from "express";
 import passport from "passport";
 
-import { handleMethodNotAllowed } from "@server/common/middleware";
 import { handleGetCsrfToken } from "@server/auth/csrf";
+import { handleMethodNotAllowed } from "@server/common/middleware";
 
 import handlers from "./auth_handlers";
 
+
 const auth_router = ExpressRouter();
 
-auth_router
-  .route("/login")
+auth_router.route("/login")
   .post(
     passport.authenticate("local", {
       failureRedirect: "/login",
@@ -23,19 +23,28 @@ auth_router.route("/signup")
   .post(handlers.handleSignUp)
   .all(handleMethodNotAllowed);
 
-auth_router
-  .route("/setpassword")
+auth_router.route("/setpassword")
   .post(handlers.handleSetPassword)
   .all(handleMethodNotAllowed);
 
-auth_router
-  .route("/csrf-token")
-  .get(handleGetCsrfToken)
+auth_router.route("/durhack-live")
+  .get(passport.authenticate("oauth2"))
   .all(handleMethodNotAllowed);
 
-auth_router
-  .route("/user")
-  .get(handlers.handleGetCurrentUser)
+auth_router.route("/durhack-live/callback")
+  .get(
+    function (request, response, next) {
+      return passport.authenticate("oauth2", {
+        failureRedirect: "/auth/login",
+        keepSessionInfo: true,
+      }).call(passport, request, response, next);
+    },
+    handlers.handleLoginSuccess,
+  )
+  .all(handleMethodNotAllowed);
+
+auth_router.route("/csrf-token")
+  .get(handleGetCsrfToken)
   .all(handleMethodNotAllowed);
 
 export default auth_router;
