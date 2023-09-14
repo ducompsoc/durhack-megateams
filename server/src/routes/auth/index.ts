7f5b3f2fb63a7f9b10bcef95c6c831d2db8c39a5
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-14 05:24:12 +0100

    implement and register necessary API methods for OAuth2 flow via DurHack Live

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -9,36 +9,17 @@ import handlers from "./auth_handlers";
 
 const auth_router = ExpressRouter();
 
-auth_router.route("/login")
-  .post(
-    passport.authenticate("local", {
-      failureRedirect: "/login",
-      failureMessage: true,
-    }),
-    handlers.handleLoginSuccess
-  )
-  .all(handleMethodNotAllowed);
-
-auth_router.route("/signup")
-  .post(handlers.handleSignUp)
-  .all(handleMethodNotAllowed);
-
-auth_router.route("/setpassword")
-  .post(handlers.handleSetPassword)
-  .all(handleMethodNotAllowed);
-
 auth_router.route("/durhack-live")
   .get(passport.authenticate("oauth2"))
   .all(handleMethodNotAllowed);
 
 auth_router.route("/durhack-live/callback")
   .get(
-    function (request, response, next) {
-      return passport.authenticate("oauth2", {
-        failureRedirect: "/auth/login",
-        keepSessionInfo: true,
-      }).call(passport, request, response, next);
-    },
+    passport.authenticate("oauth2", {
+      failureRedirect: "/auth/login",
+      keepSessionInfo: true,
+      session: true,
+    }),
     handlers.handleLoginSuccess,
   )
   .all(handleMethodNotAllowed);
