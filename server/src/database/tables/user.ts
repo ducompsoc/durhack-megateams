e367e6695f5c4663e5322e379cbec32e020dfee2
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-14 05:18:24 +0100

    remove passport, email verification, and MLH fields from user table

--- server/src/database/tables/user.ts
+++ server/src/database/tables/user.ts
@@ -1,6 +1,6 @@
 import { DataType, Table, Column, Model, BelongsTo, HasMany, ForeignKey } from "sequelize-typescript";
 
-import { UserRole, Ethnicity, Gender } from "@server/common/model_enums";
+import { UserRole } from "@server/common/model_enums";
 
 import Team from "./team";
 import Point from "./point";
@@ -40,16 +40,10 @@ export default class User extends Model {
   declare email: string;
 
   @Column({
-    type: DataType.BLOB("tiny"),
-    allowNull: true,
+    type: DataType.STRING,
+    allowNull: false,
   })
-  declare hashed_password: Buffer | null;
-
-  @Column({
-    type: DataType.BLOB("tiny"),
-    allowNull: true,
-  })
-  declare password_salt: Buffer | null;
+  declare preferred_name: string;
 
   @Column({
     type: DataType.ENUM(...Object.values(UserRole)),
@@ -57,18 +51,6 @@ export default class User extends Model {
     allowNull: false,
   })
   declare role: UserRole;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: true,
-  })
-  declare verify_code: string | null;
-
-  @Column({
-    type: DataType.DATE,
-    allowNull: true,
-  })
-  declare verify_sent_at: Date | null;
 
   @Column({
     type: DataType.DATE,
@@ -81,87 +63,6 @@ export default class User extends Model {
     allowNull: true,
   })
   declare last_logged_in_at: Date | null;
-
-  @Column({
-    type: DataType.BOOLEAN,
-    allowNull: true,
-  })
-  declare h_UK_marketing: boolean | null;
-
-  @Column({
-    type: DataType.BOOLEAN,
-    allowNull: true,
-  })
-  declare h_UK_Consent: boolean | null;
-
-  @Column({
-    type: DataType.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  })
-  declare checked_in: boolean;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: true,
-  })
-  declare discord_id: string | null;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: true,
-  })
-  declare discord_name: string | null;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
-  declare full_name: string;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
-  declare preferred_name: string;
-
-  @Column({
-    type: DataType.INTEGER,
-    allowNull: true,
-  })
-  declare age: number | null;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: true,
-  })
-  declare phone_number: string | null;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: true,
-  })
-  declare university: string | null;
-
-  @Column({
-    type: DataType.CHAR(4),
-    allowNull: true,
-  })
-  declare graduation_year: string | null;
-
-  @Column({
-    type: DataType.ENUM(...Object.values(Ethnicity)),
-    defaultValue: Ethnicity.pnts,
-    allowNull: false,
-  })
-  declare ethnicity: Ethnicity | null;
-
-  @Column({
-    type: DataType.ENUM(...Object.values(Gender)),
-    defaultValue: Gender.pnts,
-    allowNull: false,
-  })
-  declare gender: Gender | null;
 
   static async listUsers(): Promise<Pick<User, "id" | "email" | "full_name">[]> {
     throw new Error("Not implemented.");
