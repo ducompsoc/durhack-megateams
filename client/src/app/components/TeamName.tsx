fd66baa64c5b7fc8b552c2579e820656e377683f
Jacob Wrenn <jacob@wrenn.me>
2023-11-03 11:35:20 +0000

    Bug fix in team name

--- client/src/app/components/TeamName.tsx
+++ client/src/app/components/TeamName.tsx
@@ -1,6 +1,6 @@
 export default function TeamName(props: { className?: string, teamName: string }) {
-  const capitals = props.teamName.match(/([A-Z])/g) ?? [];
-  const split = props.teamName.split(/[A-Z]/);
+  const capitals = props?.teamName?.match(/([A-Z])/g) ?? [];
+  const split = props?.teamName?.split(/[A-Z]/);
   split?.shift();
 
   return (
