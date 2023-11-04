e218bcebd831877b18ac6b84824a5a8f64c59e6c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-11-04 22:12:58 +0000

    normalise megateam point totals based on number of megateam members

--- server/src/routes/megateams/megateam_handlers.ts
+++ server/src/routes/megateams/megateam_handlers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import createHttpError from "http-errors";
 import { Sequelize } from "sequelize";
+import { z } from "zod";
 
 import Megateam from "@server/database/tables/megateam";
 import Area from "@server/database/tables/area";
@@ -10,15 +11,21 @@ import Point from "@server/database/tables/point";
 
 
 class MegateamHandlers {
+  static numberParser = z.coerce.number().catch(0);
+
   async getMegateamsList(req: Request, res: Response): Promise<void> {
     const result = await Megateam.findAll({
       attributes: [
         "megateam_name",
         "megateam_description",
         [
-          Sequelize.fn("sum", Sequelize.col("areas.team.members.points.value")),
-          "points",
+            Sequelize.fn("sum", Sequelize.col("areas.team.members.points.value")),
+            "points",
         ],
+        [
+            Sequelize.fn("count", Sequelize.col("areas.team.members.user_id")),
+            "members"
+        ]
       ],
       include: [
         {
@@ -47,10 +54,15 @@ class MegateamHandlers {
       group: "Megateam.megateam_id",
     });
 
+    const most_members = Math.max(...result.map( megateam => MegateamHandlers.numberParser.parse(megateam.toJSON().members) ));
+
     const payload = result.map((megateam) => {
-      const json = megateam.toJSON();
-      json.points = (!Number.isNaN(json.points) ? parseInt(json.points) : 0) || 0;
-      return json;
+      const plain_megateam = megateam.toJSON();
+
+      const naive_points = MegateamHandlers.numberParser.parse(plain_megateam.points)
+      const members = MegateamHandlers.numberParser.parse(plain_megateam.members)
+      plain_megateam.points = Math.round(naive_points * most_members / members)
+      return plain_megateam;
     });
 
     res.json({
