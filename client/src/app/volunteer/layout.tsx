416dd358f762ec8acc621ee056647bba403e0818
root <root@KittyLaptop>
2024-07-02 21:54:07 +0100

    fixed indentation lint error

--- client/src/app/volunteer/layout.tsx
+++ client/src/app/volunteer/layout.tsx
@@ -43,7 +43,7 @@ export default function VolunteerLayout({
             },
           ]
           : []),
-          {
+        {
             icon: ScaleIcon,
             path: "/volunteer/admin",
           },
