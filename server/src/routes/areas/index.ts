ab0c27f2149378effa950fb6777cdfb3eb7468f0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-30 01:09:02 +0100

    refactor: area routes for new decorator format

--- server/src/routes/areas/index.ts
+++ server/src/routes/areas/index.ts
@@ -2,22 +2,22 @@ import { Router as ExpressRouter } from "express"
 
 import { handleFailedAuthentication, handleMethodNotAllowed, parseRouteId } from "@server/common/middleware"
 
-import handlers from "./area_handlers"
+import { areaHandlers } from "./area_handlers"
 
 const areas_router = ExpressRouter()
 
 areas_router
   .route("/")
-  .get(handlers.getAreasList, handleFailedAuthentication)
-  .post(handlers.createArea, handleFailedAuthentication)
+  .get(areaHandlers.getAreasList(), handleFailedAuthentication)
+  .post(areaHandlers.createArea(), handleFailedAuthentication)
   .all(handleMethodNotAllowed)
 
 areas_router
   .route("/:area_id")
   .all(parseRouteId("area_id"))
-  .get(handlers.getAreaDetails, handleFailedAuthentication)
-  .patch(handlers.patchAreaDetails, handleFailedAuthentication)
-  .delete(handlers.deleteArea, handleFailedAuthentication)
+  .get(areaHandlers.getAreaDetails(), handleFailedAuthentication)
+  .patch(areaHandlers.patchAreaDetails(), handleFailedAuthentication)
+  .delete(areaHandlers.deleteArea(), handleFailedAuthentication)
   .all(handleMethodNotAllowed)
 
 export default areas_router
